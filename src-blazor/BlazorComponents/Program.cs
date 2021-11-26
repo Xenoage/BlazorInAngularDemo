06d909665cd5c53bd158243326352b1098232229
Andreas Wenger <andi@xenoage.com>
2021-11-26 22:12:22 +0100

    Added Blazor routing and test pages for trying the component in a pure Blazor environment

--- src-blazor/BlazorComponents/Program.cs
+++ src-blazor/BlazorComponents/Program.cs
@@ -1,8 +1,8 @@
-using BlazorComponents.Components;
+using BlazorComponents.Test;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<Counter>("#app");
+builder.RootComponents.Add<TestApp>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
