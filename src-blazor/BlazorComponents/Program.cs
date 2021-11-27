e0a7708193379ec6a76f06d87798349729a1b31c
Andreas Wenger <andi@xenoage.com>
2021-11-27 22:26:23 +0100

    Global variable to switch between Blazor environment test mode and Angular mode

--- src-blazor/BlazorComponents/Program.cs
+++ src-blazor/BlazorComponents/Program.cs
@@ -1,15 +1,18 @@
 using AngularAdapterBuild;
+using BlazorComponents;
 using BlazorComponents.Components;
 using BlazorComponents.Test;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-/* // Use the following line when testing the pure Blazor environment:
-builder.RootComponents.Add<TestApp>("#app");
-/*/ // Use the following line for when using the component within Angular:
-builder.RootComponents.RegisterForAngular<BlazorArticleComment>();
-//*/
+if (Settings.isTestEnvironment) {
+    // Testing within the pure Blazor environment (just like a normal Blazor WebAssembly app)
+    builder.RootComponents.Add<TestApp>("#app");
+} else {
+    // Register the component for usage in Angular app
+    builder.RootComponents.RegisterForAngular<BlazorArticleComment>();
+}
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ src-blazor/BlazorComponents/Settings.cs
@@ -0,0 +1,12 @@
+namespace BlazorComponents;
+
+public static class Settings {
+
+    /// <summary>
+    /// True, to run the Blazor test environment,
+    /// false, to run within the Angular project.
+    /// Set to false before calling "dotnet publish"!
+    /// </summary>
+    public static readonly bool isTestEnvironment = false;
+
+}
