be21c04eff128abff935d8688f301729c31d60e3
Andreas Wenger <andi@xenoage.com>
2021-11-28 14:31:04 +0100

    github CI/CD with deployment to github pages; little bugfixes and welcome message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,13 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    // preload all modules; optionally we could
-    // implement a custom preloading strategy for just some
-    // of the modules (PRs welcome 😉)
-    preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
-})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
